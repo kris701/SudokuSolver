e52c060edb1685245c1e17ce491ed58cfaad5ed5
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-10 21:43:45 +0200

    Refactoring

--- SudokuSolver/Solvers/Algorithms/BacktrackSolvers/CardinalityBacktrackSolver.cs
+++ SudokuSolver/Solvers/Algorithms/BacktrackSolvers/CardinalityBacktrackSolver.cs
@@ -71,5 +71,19 @@ namespace SudokuSolver.Solvers.Algorithms.BacktrackSolvers
             }
             return null;
         }
+
+        public class CellPosition
+        {
+            public byte X;
+            public byte Y;
+            public int Possibilities;
+
+            public CellPosition(byte x, byte y, int possibilities)
+            {
+                X = x;
+                Y = y;
+                Possibilities = possibilities;
+            }
+        }
     }
 }
